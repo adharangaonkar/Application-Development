c397d2ffd638273ccfb157c695beab65bc65330b
Additya Dharangaonkar <dharangaonkar.a@husky.neu.edu>
2019-09-23 01:37:15 +0530

    Assignment 1(car model)

---
+++ car_assignment(1)/lab_2/src/Business/Car.java
@@ -0,0 +1,168 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author CCM
+ */
+public class Car {
+    private String name;
+    private String brand;
+    private String modelYear;
+    private String vehicleType;
+    private String colour;
+    private String price;
+    private String gearType;
+    private String safetyRating;
+    private String dimensions;
+    private String grdClearnace;
+    private String mileage;
+    private String maxSpeed;
+    private String fuelType;
+    private String airBags;
+    private String accessories;
+    private String warranty;
+    private String path;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+
+    public String getWarranty() {
+        return warranty;
+    }
+
+    public void setWarranty(String warranty) {
+        this.warranty = warranty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModelYear() {
+        return modelYear;
+    }
+
+    public void setModelYear(String modelYear) {
+        this.modelYear = modelYear;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getGearType() {
+        return gearType;
+    }
+
+    public void setGearType(String gearType) {
+        this.gearType = gearType;
+    }
+
+    public String getSafetyRating() {
+        return safetyRating;
+    }
+
+    public void setSafetyRating(String safetyRating) {
+        this.safetyRating = safetyRating;
+    }
+
+    public String getDimensions() {
+        return dimensions;
+    }
+
+    public void setDimensions(String dimensions) {
+        this.dimensions = dimensions;
+    }
+
+    public String getGrdClearnace() {
+        return grdClearnace;
+    }
+
+    public void setGrdClearnace(String grdClearnace) {
+        this.grdClearnace = grdClearnace;
+    }
+
+    public String getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(String mileage) {
+        this.mileage = mileage;
+    }
+
+    public String getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(String maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public String getAirBags() {
+        return airBags;
+    }
+
+    public void setAirBags(String airBags) {
+        this.airBags = airBags;
+    }
+
+    public String getAccessories() {
+        return accessories;
+    }
+
+    public void setAccessories(String accessories) {
+        this.accessories = accessories;
+    }
+    
+}
